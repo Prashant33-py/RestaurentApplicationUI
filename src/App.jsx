b9039fc6ad295b8f2e7ba1dd718cf648d2310893
Prashant33-py <pvasala786@gmail.com>
2025-04-04 21:36:59 +0530

    Adds empty navbar

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import { FoodItem } from "./components/FoodItem";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { GetAllFoodItems } from "./components/GetAllFoodItems";
 import { AddFoodItem } from "./components/AddFoodItem";
+import Navbar from "./components/Navbar";
 import "./App.css";
 
 function App() {
 	return (
 		<>
+			<Navbar />
 			<BrowserRouter>
 				<Routes>
 					<Route
